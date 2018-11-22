88069b539998cf8f17e0dd35d6aa3821e741baa1
Janine Luk <janineluk95@gmail.com>
2018-11-22 15:22:39 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,22 +2,12 @@
 
 class Person
   
-  attr_writer :bank_account, :happiness
+  attr_writer :name, :bank_account, :happiness
   
   def initialize(name)
     @name = name
+    @bank_account
   end
   
-  def name
-    @name
-  end
-  
-  def bank_account
-    @bank_account = 25
-  end
-  
-  def happiness
-    @happiness = 8
-  end
-  
+
 end
