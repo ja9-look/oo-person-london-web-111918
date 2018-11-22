aa35e734e43f0d6c926b3c7cf77d1fbfdc280854
Janine Luk <janineluk95@gmail.com>
2018-11-22 15:55:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -70,6 +70,8 @@ class Person
       self.happiness -= 2
       friend.happiness -= 2
     return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+    return "blah blah sun blah rain"
   end
   end
 
